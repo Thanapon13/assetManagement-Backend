84a2ff1ce709a05ffa076ea5875b30c3a836c225
จิตติพัฒน์ วงศ์ภูมิ <jittiphat.won@devdeva.tech>
2023-07-17 10:32:18 +0700

    add model transfer

---
+++ models/subCompomentTransferModel.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, Sequelize) => {
+  const subComponentTranfer = sequelize.define(
+    "TB_SUB_COMPONENT_TRANSFERS ",
+    {
+      // ด้านล่างเป็นการตั้งค่า attribute ของ table นะครับ
+      // ชื่อตัวแปรที่เราใช้เรียกแทน: { type: Sequelize.STRING(50), allowNull: false, field: 'ชื่อของ attribute' }
+      // สามารถใส่ option เพิ่มเติมได้นะครับเช่น primaryKey: true อะไรแบบนี้
+      // แล้วก็อันนี้สำคัญ ** ไม่จำเป็นต้องสร้าง attribute ที่เป็น FK จาก table อื่นนะครับ เพราะเราจะไปกำหนด relation กันใน file index
+      _id: {
+        type: Sequelize.INTEGER(11),
+        primaryKey: true,
+        autoIncrement: true,
+        field: "_id",
+      },
+      assetNumber: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "assetNumber",
+      },
+      isPackage: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true,
+        field: "isPackage",
+      },
+      productName: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "productName",
+      },
+      amount: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        field: "amount",
+      },
+
+      // floorId: {
+      //   type: mongoose.Schema.Types.ObjectId,
+      //   ref: "Floor",
+      //   required: true,
+      // },
+    },
+    {
+      tableName: "TB_SUB_COMPONENT_TRANSFERS ",
+    }
+  );
+
+  return subComponentTranfer;
+};
---
+++ models/transferModel.js
@@ -0,0 +1,99 @@
+module.exports = (sequelize, Sequelize) => {
+  const transfer = sequelize.define(
+    "TB_TRANSFERS",
+    {
+      // ด้านล่างเป็นการตั้งค่า attribute ของ table นะครับ
+      // ชื่อตัวแปรที่เราใช้เรียกแทน: { type: Sequelize.STRING(50), allowNull: false, field: 'ชื่อของ attribute' }
+      // สามารถใส่ option เพิ่มเติมได้นะครับเช่น primaryKey: true อะไรแบบนี้
+      // แล้วก็อันนี้สำคัญ ** ไม่จำเป็นต้องสร้าง attribute ที่เป็น FK จาก table อื่นนะครับ เพราะเราจะไปกำหนด relation กันใน file index
+      _id: {
+        type: Sequelize.INTEGER(11),
+        primaryKey: true,
+        autoIncrement: true,
+        field: "_id",
+      },
+      transferSector: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "transferSector",
+      },
+      subSector: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "subSector",
+      },
+      transfereeSector: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        unique: true,
+        field: "transfereeSector",
+      },
+      building: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "building",
+      },
+      floor: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "floor",
+      },
+      room: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "room",
+      },
+      name_recorder: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "name_recorder",
+      },
+      dateTime_recorder: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        field: "dateTime_recorder",
+      },
+      name_courier: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "name_courier",
+      },
+      dateTime_courier: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        field: "dateTime_courier",
+      },
+      name_approver: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "name_approver",
+      },
+      dateTime_approver: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        field: "name_approver",
+      },
+      status: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        field: "status",
+      },
+     
+      reason: {
+        type: Sequelize.STRING(50),
+        allowNull: true,
+        field: "reason",
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        field: "deletedAt",
+      },
+    },
+    {
+      tableName: "TB_TRANSFERS",
+    }
+  );
+
+  return transfer;
+};
